ab5aa97e134ec7b27cc8ed3e00581075c1c1fa07
Kunwoo Shin <kunwooshin97@gmail.com>
2024-11-18 14:17:25 +0900

    Update main.py
    terminate the chatbot when user entered 'exit'

--- askgeo/main.py
+++ askgeo/main.py
@@ -19,6 +19,9 @@ def main():
         # user_prompt = 'Leonard Gordon Park 자전거 정류장 좌표알려줘'  # 정확한 명칭 입력했다고 가정
         user_prompt = '공원의 자전거 정류장 좌표알려줘 공원 이름은 내가 알려줄게' # one user input + one geodb query
         response = rag.start_chat(user_prompt)
+        if response == 'exit':
+            print('Askgeo terminated')
+            exit(-1)
         print(response)
         exit(-1)
 
