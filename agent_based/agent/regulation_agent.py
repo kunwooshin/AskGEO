82dced245a8584049263743dd1824aeb99eec157
Kunwoo Shin <kunwooshin97@gmail.com>
2024-12-05 17:36:51 +0900

    Add load_dotenv()

--- agent_based/agent/regulation_agent.py
+++ agent_based/agent/regulation_agent.py
@@ -1,10 +1,12 @@
 import os
-
-from openai import OpenAI
 from sqlalchemy import create_engine, text
 from sqlalchemy.exc import SQLAlchemyError
 
+from dotenv import load_dotenv
+from openai import OpenAI
+
 # Initialize OpenAI
+load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 llm = OpenAI(api_key=OPENAI_API_KEY)
